7cec5ba0a29535748db058aa60fdf654847e7467
KevinJohansen17 <kevinrondon531@gmail.com>
2026-07-31 10:46:57 -0500

    Aplicacion configurando las variables de entorno

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ app.get("/", (_, res) => {
   res.send(`Hola, estoy aprendiendo express, ficha 3407181 ADSO en el sena`, );
 });
 
+app.get("/otraruta", (req, res) => {
+    res.send(`<h1> Otro ejemplo de ruta </h1>
+        <h2> End point con res.send </h2>`)
+})
+
 app.listen(3000, () => {
   console.log(`Servidor en funcionamiento en el puerto: http://localhost:3000`);
 });
