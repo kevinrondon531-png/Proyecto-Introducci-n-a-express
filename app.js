889d1245a6b064c15211837931a9006c3442179e
Juan-Beleno <Juanchobele16@gmail.com>
2026-08-21 11:27:13 -0500

    primer endpoint de lista de aprendices

--- app.js
+++ app.js
@@ -2,11 +2,27 @@ const express = require("express")
 const app = express()
 require("dotenv/config")
 const puerto = process.env.PUERTO || 3000
+//configurar para la lectura del archivo
+const sistemaArchivo = require("fs")
+const ruta = require("path")
+const rutaArchivoJson = ruta.join(__dirname, "datos.json")
 
 //endpoint raiz
-app.get("/", (_, res) => {
+app.get("/", function(req, res){
   res.send(`API Rest - Aprendices`);
-});
+})
+
+//endpoint para ver los datos del archivo
+app.get("/api/aprendices", (req, res) => {
+  //datos vienen del archivo
+  sistemaArchivo.readFile(rutaArchivoJson, "utf-8", (error, datos)=>{
+    if(error){
+      return res.json({Error: "No se puede leer los datos"})
+    }
+    const ListaAprendices = JSON.parse(datos)
+    res.json(ListaAprendices)
+  })
+})
 
 
 app.listen(puerto, () => {
