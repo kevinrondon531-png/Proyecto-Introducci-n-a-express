5723fbba728426cb7e6b8fc57144397dfe277dc1
Juan-Beleno <Juanchobele16@gmail.com>
2026-08-21 10:44:01 -0500

    Creamos la Rama Aprendices en el primer commit en main

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (_, res) => {
-  res.send(`Hola, estoy aprendiendo express, ficha 3407181 ADSO en el sena`, );
+  res.send(`API Rest`, );
 });
 
 app.listen(3000, () => {
