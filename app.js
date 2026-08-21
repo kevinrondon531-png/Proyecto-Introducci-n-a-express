5581ac088d5b824ae4ae5d4625663ff37e486e79
Juan-Beleno <Juanchobele16@gmail.com>
2026-08-21 11:07:04 -0500

    Creamos el archivo .gitignore, modificamos el codigo y corremos el servidor con DEV

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express")
 const app = express()
+require("dotenv/config")
+const puerto = process.env.PUERTO || 3000
 
+//endpoint raiz
 app.get("/", (_, res) => {
-  res.send(`API Rest`, );
+  res.send(`API Rest - Aprendices`);
 });
 
-app.listen(3000, () => {
-  console.log(`Servidor en funcionamiento en el puerto: http://localhost:3000`);
+
+app.listen(puerto, () => {
+  console.log(`Servidor en funcionamiento en el puerto: http://localhost:${puerto}`);
 });
